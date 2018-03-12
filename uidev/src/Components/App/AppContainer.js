d73e130a703ea640107460437534fc1d75ca9a6c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-12 19:45:10 +0900

    #11 Registering Local Node with Master Node

--- uidev/src/Components/App/AppContainer.js
+++ uidev/src/Components/App/AppContainer.js
@@ -3,6 +3,7 @@ import { injectGlobal } from "styled-components";
 import reset from "styled-reset";
 import axios from "axios";
 import typography from "../../typography";
+import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE } from "../../constants";
 import AppPresenter from "./AppPresenter";
 
 const baseStyles = () => injectGlobal`
@@ -17,10 +18,19 @@ class AppContainer extends Component {
   state = {
     isLoading: true
   };
+  componentDidMount = () => {
+    this._registerOnMaster();
+  };
   render() {
     baseStyles();
     return <AppPresenter {...this.state} />;
   }
+  _registerOnMaster = () => {
+    const request = axios.post(`${MASTER_NODE}/peers`, {
+      peer: SELF_P2P_NODE
+    });
+    console.log(request);
+  };
 }
 
 export default AppContainer;
--- uidev/src/constants.js
+++ uidev/src/constants.js
@@ -1,2 +1,5 @@
 export const MASTER_NODE = "http://localhost:4000";
 export const SELF_NODE = `http://localhost:${window.sharedPort}`;
+export const SELF_P2P_NODE = `ws://localhost:${window.sharedPort}`;
+
+console.log(window);
